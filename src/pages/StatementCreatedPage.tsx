a7c5822bd172ab1c55de4e80a53682e8887b2c3e
dimastepanov <dimastepanov1980@gmail.com>
2025-05-08 13:17:57 +0700

    Final PDF

--- src/pages/StatementCreatedPage.tsx
+++ src/pages/StatementCreatedPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const StatementCreatedPage = ({ onSendEmail, onDone }: { onSendEmail: () => void, onDone: () => void }) => {
