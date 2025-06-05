c627da9d5ac1436ca3078a3809bc533a63b94676
dimastepanov <dimastepanov1980@gmail.com>
2025-06-05 21:29:32 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,18 @@ const CACHE_NAME = 't-wallet-v1';
 
 // Добавляем все возможные маршруты приложения
 const ROUTES = [
-  '/',
-  '/login',
-  '/password',
-  '/home',
-  '/top-up',
-  '/card-transfer',
-  '/payments',
-  '/city',
-  '/chat',
-  '/more',
-  '/add-account',
-  '/add-account/new-account',
+  './',
+  './login',
+  './password',
+  './home',
+  './top-up',
+  './card-transfer',
+  './payments',
+  './city',
+  './chat',
+  './more',
+  './add-account',
+  './add-account/new-account',
 ];
 
 self.addEventListener('install', (event) => {
@@ -23,28 +23,28 @@ self.addEventListener('install', (event) => {
       .then((cache) => {
         // Сначала кэшируем основные файлы
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
+          './',
+          './index.html',
+          './manifest.json',
           // Используем iOS иконки как основные
-          '/ios/32.png',  // для favicon
-          '/ios/72.png',
-          '/ios/96.png',
-          '/ios/128.png',
-          '/ios/144.png',
-          '/ios/152.png',
-          '/ios/192.png',
-          '/ios/256.png',
-          '/ios/512.png',
+          './ios/32.png',  // для favicon
+          './ios/72.png',
+          './ios/96.png',
+          './ios/128.png',
+          './ios/144.png',
+          './ios/152.png',
+          './ios/192.png',
+          './ios/256.png',
+          './ios/512.png',
           // Добавляем ссылку на favicon
-          { '/favicon.ico': '/ios/32.png'}
+          { './favicon.ico': './ios/32.png'}
         ]);
       })
       .then(async () => {
         // Затем пытаемся закэшировать все assets
         const cache = await caches.open(CACHE_NAME);
         try {
-          const response = await fetch('/asset-manifest.json');
+          const response = await fetch('./asset-manifest.json');
           const assets = await response.json();
           const urlsToCache = Object.values(assets);
           console.log('asset-manifest', assets);
@@ -69,7 +69,7 @@ self.addEventListener('fetch', (event) => {
 
         // Если это навигационный запрос (HTML страница)
         if (event.request.mode === 'navigate') {
-          return caches.match('/index.html');
+          return caches.match('./index.html');
         }
 
         // Иначе делаем запрос к серверу
@@ -92,7 +92,7 @@ self.addEventListener('fetch', (event) => {
           .catch(() => {
             // Если запрос не удался и это HTML страница
             if (event.request.mode === 'navigate') {
-              return caches.match('/index.html');
+              return caches.match('./index.html');
             }
             // Для остальных ресурсов возвращаем "заглушку"
             return new Response('Offline');
